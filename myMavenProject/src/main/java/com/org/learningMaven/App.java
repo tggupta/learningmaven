c65fa7cc2cbefe3a8b978a4c8dc0e4499f804e5e
tggupta <tggupta22@gmail.com>
2017-05-25 01:42:17 +0530

    mmmm

---
+++ myMavenProject/src/main/java/com/org/learningMaven/App.java
@@ -0,0 +1,13 @@
+package com.org.learningMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ myMavenProject/src/test/java/com/org/learningMaven/test.java
@@ -0,0 +1,12 @@
+package com.org.learningMaven;
+
+import org.testng.annotations.Test;
+
+public class test {	
+	@Test
+	public void getlogin()
+	{
+		System.out.println("loginnnnnnnnnnnnn***************************************");
+	}
+
+}
