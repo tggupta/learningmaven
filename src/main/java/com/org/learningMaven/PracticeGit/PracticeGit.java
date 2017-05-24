f3682acd021959f57766d9eda44578a5383ce176
tggupta <tggupta22@gmail.com>
2017-05-25 00:11:33 +0530

    wew

--- src/main/java/com/org/learningMaven/PracticeGit/PracticeGit.java
+++ src/main/java/com/org/learningMaven/PracticeGit/PracticeGit.java
@@ -4,7 +4,7 @@ public class PracticeGit {
 
 	public static void main(String[] args) {
 		System.out.println("Practice Git and Githubs");
-		System.out.println("practice11");
+		System.out.println("practice1122");
 
 	}
 
