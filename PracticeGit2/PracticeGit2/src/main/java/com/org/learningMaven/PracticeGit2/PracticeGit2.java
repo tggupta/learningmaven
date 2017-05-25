343bd27803c59ed1c9e95b75304cfdb1dd8d8da6
tggupta <tggupta22@gmail.com>
2017-05-25 23:39:38 +0530

    2

--- PracticeGit2/PracticeGit2/src/main/java/com/org/learningMaven/PracticeGit2/PracticeGit2.java
+++ PracticeGit2/PracticeGit2/src/main/java/com/org/learningMaven/PracticeGit2/PracticeGit2.java
@@ -3,7 +3,7 @@ package com.org.learningMaven.PracticeGit2;
 public class PracticeGit2 {
 
 	public static void main(String[] args) {
-	System.out.println("this is git");
+	System.out.println("this iis git");
 
 	}
 
