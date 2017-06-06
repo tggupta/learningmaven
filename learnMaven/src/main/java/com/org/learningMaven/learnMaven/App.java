5919658f583b287a3a2eab3173ea09f973e3e90c
tggupta <tggupta22@gmail.com>
2017-06-06 23:36:03 +0530

    moving to stage v1

---
+++ learnMaven/src/main/java/com/org/learningMaven/learnMaven/App.java
@@ -0,0 +1,13 @@
+package com.org.learningMaven.learnMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ learnMaven/src/main/java/com/org/learningMaven/learnMaven/staticPayment.java
@@ -0,0 +1,19 @@
+package com.org.learningMaven.learnMaven;
+
+import org.testng.annotations.Test;
+
+public class staticPayment {
+		@Test
+		public void getPaymentFlipkart(){
+		System.out.println("Flipkart - Please complete the payment process");	
+		}
+		@Test
+		public void getPaymentAmazon(){
+		System.out.println("Amazon - Please complete the payment process");	
+		}
+		@Test
+		public void getPaymentSnapdeal(){
+		System.out.println("Snapdeal - Please complete the payment process");	
+		}
+	}
+
